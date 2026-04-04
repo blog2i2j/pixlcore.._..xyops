58be32f53d7ae29c08a354fe4610654155a293b0
Joseph Huckaby <jhuckaby@pixlcore.com>
2026-04-03 18:33:14 -0700

    Meta: Add local bin/release.sh script for automating releases.

---
+++ bin/release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+# Release the latest version of xyOps
+
+HOMEDIR="$(dirname "$(cd -- "$(dirname "$0")" && (pwd -P 2>/dev/null || pwd))")"
+cd $HOMEDIR
+
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+SHORTVER="v$PACKAGE_VERSION"
+LONGVER="Version $PACKAGE_VERSION"
+echo $LONGVER
+git tag -a "$SHORTVER" -m "$LONGVER" && git push --tags
+
+node bin/changelog.js
